3f1fdaa3ddcb2ec035f5a76453249ab2a558f162
Murtazaweb <beast.onmode20@gmail.com>
2018-03-16 12:16:17 +0500

    'aaaaa'

---
+++ src/components/newQuote.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="row">
+  <form>
+    <div class="col-sm-8 col-sm-offset-2">
+      <label>Notes</label>
+      <textarea class="form-control" v-model="note"></textarea>
+    </div>
+    <div class="col-sm-8 col-sm-offset-2">
+      <button class="btn btn-primary" @click.prevent="newNote">Add new Note</button>
+
+    </div>
+  </form>
+</div>
+</template>
+
+<script>
+    export default {
+      data(){
+        return{
+          note:''
+        }
+      },
+      methods:{
+        newNote(){
+          this.$emit('noteAdded', this.note);
+          this.note = ''
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
